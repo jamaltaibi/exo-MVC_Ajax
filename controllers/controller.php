e6947e0d2ccfa8228c90de787061c3790496da03
Your Name <jamaltaibi@MacBook-Pro-de-Jamal.local>
2024-02-29 16:02:13 +0100

    Exo MVC_Ajax

--- controllers/controller.php
+++ controllers/controller.php
@@ -1,17 +1,21 @@
 <?php
     require_once "models/model.php";
-    
-    class controller{
+        
+    class controller{        
+         /** * Permet d'afficher le header et le debut du DocType html la base. */
         function index(){
             require_once "views/header.php";
             require_once "views/base.php";
-        }
+        }        
+        /** * Affiche le tableau des Taches*/
         function affichageTaches(){
             require_once 'views/tabaffichage.php';
         }
+        /** * Affiche le champs a remplir des Taches */
         function soumettreTache(){
             require "views/tabtache.php";
         }
+        /** * Fonction qui permet au controlleur d'acceder a la base de données afin d'Inserer la donnée. */
         function entrerTache(){
             if(isset($_POST['tache'])) {
                 $tache = htmlspecialchars($_POST['tache']);
@@ -19,6 +23,7 @@
                 $conn->insertBdd($tache);
             } 
         }
+        /** * Fonction qui permet au controlleur d'acceder a la base de données afin de Supprimer la donnée. */
         function supptache(){
             $suppTache = $_POST["idTache"];
             $conn = new Bdd;
--- models/model.php
+++ models/model.php
@@ -1,9 +1,11 @@
 <?php 
 
+    /** * Accés a la base de donnéés et au fonction de la BDD */
     class Bdd{
 
         public $connexion;
 
+        /** * Permet d'acceder a la base de données */
         public function __construct() {
             $dsn = "mysql:host=localhost;dbname=test";
             $username = "root";
@@ -12,12 +14,17 @@
             $this->connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             // echo "vous êtes connecté à la bdd";
         }
+        /** * Permet de recuperer et lire la base de données choisie */
         function readAll(){
             $sql = "SELECT * FROM `Taches`";
             $request = $this->connexion->prepare($sql);
             $request->execute();
             return $request->fetchAll(PDO::FETCH_ASSOC);
         }
+        /**
+         * Permet d'inserer dans la base de données choisie
+         * @param $param1 $param1 [la tache a effectué] 
+         * */
         function insertBdd($param1){
             $sql = "INSERT INTO `Taches`(`tache`) VALUES (:tache)";
             $stmtinsert = $this->connexion->prepare($sql);
@@ -25,6 +32,11 @@
             $stmtinsert->execute();
             echo "data bien ajouté à la bdd";
         }
+        /**
+         * Permet de Modifier dans la base de données choisie
+         * @param $idTache $idTache ['']
+         * @param $nouvelleTache $nouvelleTache [Modifie la tache enregistrer]
+         */
         function modifBdd($idTache, $nouvelleTache){
             $sql = "UPDATE `Taches` SET `tache` = :nouvelleTache WHERE `id` = :idTache";
             $stmt = $this->connexion->prepare($sql);
@@ -32,6 +44,10 @@
             $stmt->bindParam(':idTache', $idTache);
             $stmt->execute();
         }
+        /**
+         * Permet de Supprimer dans la base de données choisie
+         * @param $suppTache $suppTache [Supprime l'id et la tache]
+         */
         function suppBdd($suppTache){
             $sqlSup = "DELETE FROM `Taches` WHERE id = :idTache";
             $stmtSup = $this->connexion->prepare($sqlSup);
